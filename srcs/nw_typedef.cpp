2c153b187f4ad8e913afee63e1a217b3fc2aca8f
Benoit HIVERT <hivert.benoit@gmail.com>
2022-03-12 20:49:53 +0100

    typedef update

--- srcs/nw_typedef.cpp
+++ srcs/nw_typedef.cpp
@@ -9,25 +9,17 @@
 #include "nw_typedef.hpp"
 
 static const std::map<const nw::sa_family, const std::string>	s_family_str = {
-	{nw::sa_family::INET,	"AF_INET"},
-	{nw::sa_family::INET6,	"AF_INET6"},
-	{nw::sa_family::UNSPEC,	"AF_UNSPEC"}
+	{nw::sa_family::UNSPEC,		"AF_UNSPEC"},
+	{nw::sa_family::INET,		"AF_INET"},
+	{nw::sa_family::INET6,		"AF_INET6"}
 };
 
 const std::string &	nw::sa_family_str(const nw::sa_family &family) {
 	return s_family_str.at(family);
 }
 
-//static const std::map<const nw::sock_use, const std::string>	s_use_str = {
-//	{nw::sock_use::CONNECT,	"CONNECT"},
-//	{nw::sock_use::BIND,		"BIND"},
-//};
-//
-//const std::string &	nw::sock_use_str(const nw::sock_use &use) {
-//	return s_use_str.at(use);
-//}
-
 static const std::map<const nw::sock_type, const std::string>	s_type_str = {
+	{nw::sock_type::UNSPEC,		"UNSPEC"},
 	{nw::sock_type::STREAM,		"STREAM"},
 	{nw::sock_type::DGRAM,		"DGRAM"},
 	{nw::sock_type::SEQPACKET,	"SEQPACKET"},
