e217e2d9b2df964b9ffa2af72370cd590d9eb5d5
Benoit HIVERT <hivert.benoit@gmail.com>
2022-03-12 20:47:58 +0100

    addr update with _sizeof

--- srcs/nw_addr.hpp
+++ srcs/nw_addr.hpp
@@ -16,6 +16,9 @@
 # include <arpa/inet.h>
 
 namespace nw {
+	template <sa_family FAMILY>
+	class socket_interface;
+
 	class addr_storage {
 		public:
 			typedef struct sockaddr_storage	type;
@@ -28,10 +31,11 @@ namespace nw {
 
 		protected:
 			const type			_struct;
+			const socklen_type	_sizeof;
 
-			addr_storage(void) : _struct{static_cast<sa_family_t>(sa_family::UNSPEC), {0},0} {}
+			addr_storage(void) : _struct{static_cast<sa_family_t>(sa_family::UNSPEC), {0},0}, _sizeof(sizeof(type)) {}
 
-			virtual	~addr_storage(void) {};
+			virtual	~addr_storage(void) {}
 
 			const type &	get_ref(void) const {
 				return this->_struct;
@@ -64,15 +68,9 @@ namespace nw {
 		public:
 			typedef struct sockaddr_in				type;
 
-			class invalid_conversion : public nw::exception {
-				public:
-					const char	*what(void) const noexcept {
-						return "invalid conversion to inet family";
-					}
-			};
-
 			addr(void) : _struct(reinterpret_cast<const type &>(get_ref())) {
 				const_cast<type &>(this->_struct).sin_family = AF_INET;
+				const_cast<socklen_type &>(this->_sizeof) = sizeof(type);
 			}
 
 			addr(const port_type &port, const std::string &addr = "0.0.0.0") : addr::addr() {
@@ -95,7 +93,7 @@ namespace nw {
 
 			addr(const addr_storage &src) : addr(static_cast<const addr<sa_family::INET> &>(src)._struct) {
 				if (this->_struct.sin_family != static_cast<sa_family_t>(sa_family::INET))
-					throw invalid_conversion();
+					throw logic_error("addr_storage: invalid conversion");
 			}
 
 			virtual	~addr(void) {}
@@ -115,7 +113,7 @@ namespace nw {
 
 				str = "{ \"family\": \"" + sa_family_str(static_cast<sa_family>(this->_struct.sin_family)) + "\", ";
 				str += "\"port\": " + std::to_string(ntohs(this->_struct.sin_port)) + ", ";
-				str += "\"address\": \"" + std::string(inet_ntoa(this->_struct.sin_addr)) + "\" }";
+				str += "\"ip\": \"" + std::string(inet_ntoa(this->_struct.sin_addr)) + "\" }";
 
 				return str;
 			}
@@ -124,6 +122,9 @@ namespace nw {
 			const type	&_struct;
 
 			friend addr<sa_family::UNSPEC>;
+
+			template <sa_family FAMILY>
+			friend class socket_interface;
 
 		private:
 	};
@@ -135,6 +136,7 @@ namespace nw {
 
 			addr(void) : _struct(reinterpret_cast<const type &>(get_ref())) {
 				const_cast<type &>(this->_struct).sin6_family = AF_INET6;
+				const_cast<socklen_type &>(this->_sizeof) = sizeof(type);
 			}
 
 			addr(const port_type &port, const std::string &addr = "::", uint32_t flowinfo = 0, uint32_t scope_id = 0) : addr::addr() {
@@ -150,7 +152,7 @@ namespace nw {
 				if (!(ret = inet_pton(AF_INET6, addr.c_str(), &ref.sin6_addr)))
 					throw logic_error("inet_pton: invalid address");
 				else if (ret == -1)
-					throw system_error(errno,std::generic_category());
+					throw system_error(errno, std::generic_category(), "inet_pton");
 			}
 
 			addr(const type &addr) : addr::addr() {
@@ -184,7 +186,7 @@ namespace nw {
 				str = "{ \"family\": \"" + sa_family_str(static_cast<sa_family>(this->_struct.sin6_family)) + "\", ";
 				str += "\"port\": " + std::to_string(ntohs(this->_struct.sin6_port)) + ", ";
 				str += "\"flowinfo\": " + std::to_string(ntohl(this->_struct.sin6_flowinfo)) + ", ";
-				str += "\"address\": \"" + std::string(inet_ntop(AF_INET6, &this->_struct.sin6_addr, reinterpret_cast<char *>(&addr), sizeof(type))) + "\", ";
+				str += "\"ipv6\": \"" + std::string(inet_ntop(AF_INET6, &this->_struct.sin6_addr, reinterpret_cast<char *>(&addr), sizeof(type))) + "\", ";
 				str += "\"scope_id\": " + std::to_string(ntohl(this->_struct.sin6_scope_id)) + " }";
 
 				return str;
@@ -194,6 +196,9 @@ namespace nw {
 			const type	&_struct;
 
 			friend addr<sa_family::UNSPEC>;
+
+			template <sa_family FAMILY>
+			friend class socket_interface;
 
 		private:
 	};
@@ -233,18 +238,22 @@ namespace nw {
 
 			addr(void) : _struct(reinterpret_cast<const type &>(get_ref())) {
 				const_cast<type &>(this->_struct).ss_family = AF_UNSPEC;
+				const_cast<socklen_type &>(this->_sizeof) = sizeof(type);
 			}
 
 			addr(const type &addr) : addr::addr() {
 				switch (static_cast<sa_family>(addr.ss_family)) {
 					default:
 						const_cast<type &>(this->_struct) = addr;
+						const_cast<socklen_type &>(this->_sizeof) = sizeof(type);
 						break ;
 					case sa_family::INET:
 						reinterpret_cast<nw::addr<sa_family::INET>::type &>(const_cast<type &>(this->_struct)) = reinterpret_cast<const nw::addr<sa_family::INET>::type &>(addr);
+						const_cast<socklen_type &>(this->_sizeof) = sizeof(nw::addr<sa_family::INET>::type);
 						break ;
 					case sa_family::INET6:
 						reinterpret_cast<nw::addr<sa_family::INET6>::type &>(const_cast<type &>(this->_struct)) = reinterpret_cast<const nw::addr<sa_family::INET6>::type &>(addr);
+						const_cast<socklen_type &>(this->_sizeof) = sizeof(nw::addr<sa_family::INET6>::type);
 						break ;
 				}
 			}
@@ -272,7 +281,7 @@ namespace nw {
 
 				switch (this->get_family()) {
 					default:
-						str = "{ \"family\" :\"" + sa_family_str(sa_family::UNSPEC) + " }";
+						str = "{ \"family\" :\"" + sa_family_str(sa_family::UNSPEC) + "\" }";
 						break ;
 					case sa_family::INET:
 						str = addr<sa_family::INET>(*this).to_string();
